d48763d29e424eba07d2737d58a9491b97aa8ff3
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-26 23:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  document.querySelector('ul.employee-list').appendChild(li);
+}
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
