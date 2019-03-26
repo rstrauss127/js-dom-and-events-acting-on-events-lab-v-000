5cb53d8e34b0239e804974ca352346e168fc77a7
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-26 23:41:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function addNewElementAsLi() {
 
 function addNewLiOnClick(){
   document.querySelector('form input:last-child').addEventListener('click', addNewElementAsLi());
-  document.querySelector('input').value.remove();
+  document.querySelector('input').innerHTML.remove();
 }
 
 function preventRefreshOnSubmit(){
