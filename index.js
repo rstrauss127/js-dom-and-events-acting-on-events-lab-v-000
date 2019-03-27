2b3a717b0df3bb415bcf248c5c42ce9971aaea5d
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-27 00:00:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function addNewLiOnClick(){
 
 function clearEmployeeListOnLinkClick() {
   document.querySelector('a').addEventListener('click', function(c) {
-    let ba = document.querySelectorAll('ul.employee-list li)';
+    let ba = document.querySelectorAll('ul.employee-list)';
     for(let i=0; i < ba.length; i++) {
       ba = "";
     }
