908876271a74663512cdea63a17c85023ea681b2
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-27 00:10:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  document.querySelector('ul.employee-list').appendChild(li);
+}
+
+function addNewLiOnClick(){
+  document.querySelector('form input:last-child').addEventListener('click', addNewElementAsLi());
+  document.querySelector('input').value = "";
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(c) {
+    document.querySelector('ul').innerHTML = "";
+  })
+}
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
     })
 }
+
 
 preventRefreshOnSubmit()
